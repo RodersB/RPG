2dbf1d5e91523c81da2960c08d2e005c69157a85
Rodrigo <rodrigofrb@hotmail.com>
2020-08-06 01:05:27 +0100

    new

--- character.py
+++ character.py
@@ -1,9 +1,63 @@
+def get_base_stats():
+    return {
+        "base_stats": {
+            "hp": 100,
+            "mp": 100,
+            "hp_regen": 5,
+            "mp_regen": 2,
+            "hp_leech": 0,
+            "mp_leech": 0,
+            "base_attack_speed": 100,
+            "critical_chance": 0,
+            "dodge_chance": 0,
+            "block_chance": 0,
+            "block_dmg_reduction": 0,
+            "armor": 10,
+            "magic_resist": 10
+        },
+        "modded_stats": {
+            "hp": 100,
+            "mp": 100,
+            "hp_regen": 5,
+            "mp_regen": 2,
+            "hp_leech": 0,
+            "mp_leech": 0,
+            "base_attack_speed": 100,
+            "critical_chance": 0,
+            "dodge_chance": 0,
+            "block_chance": 0,
+            "block_dmg_reduction": 0,
+            "armor": 10,
+            "magic_resist": 10
+        }
+    }
+
+
+def get_base_gear():
+    return {
+        "helm": None,
+        "body_armor": None,
+        "main_hand": None,
+        "off_hand": None,
+        "gloves": None,
+        "boots": None,
+        "ring1": None,
+        "ring2": None,
+        "amulet": None
+    }
+
+
 class Character:
-    def __init__(self, name, stats):
+    def __init__(self, name, stats=get_base_stats(), play_time=0, gear=get_base_gear(), inventory=[]):
         self.name = name
         self.stats = stats
-        self.gear = {}
-        self.inventory = []
+        self.play_time = play_time
+        self.gear = gear
+        self.inventory = inventory
+
+    @classmethod
+    def init_from_json(cls, c):
+        return cls(name=c["name"], stats=c["stats"], play_time=c["play_time"], gear=c["gear"], inventory=c["inventory"])
 
     def equip_item(self, equipment):
         if self.gear.get(equipment.slot, None) is not None:
@@ -14,5 +68,3 @@ class Character:
 
     def calculate_stats(self):
         pass
-
-
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from character import Character
 from encounter import Encounter
 from equipment import Equipment
 from gem import Gem
+import json
 
 
 def get_items():
@@ -17,35 +18,79 @@ def get_gems():
     mana_gem = Gem(1, 'Gem of Mana', {'flat_mana': 10})
     as_gem = Gem(1, 'Gem of Speed', {'increased_attack_speed': 5})
 
-
-def get_character():
-    base_stats = {
-        'hp': 100,
-        'mana': 100,
-        'level': 1,
-        'current_xp': 0,
-    }
-
-    items = get_items()
-
-    player = Character('TestChar', base_stats)
-    player.equip_item(items[0])
-    player.equip_item(items[1])
-    player.equip_item(items[2])
-    player.equip_item(items[3])
-
-
 def run_encounter(player, encounter_config):
     pass
 
+def get_user_input(txt):
+    user_input = input(txt + "\n Type 'Q' to abort.")
+
+def new_char(existing_characters):
+    invalid_name = True
+    abort = False
+    c = None
+    while invalid_name and not abort:
+        char_name = input("Stranger: Tell me your name ('Q' to abort)")
+        if char_name.upper() == "Q":
+            abort = True
+            c = None
+        else:
+            if len(char_name) > 2:
+                if char_name not in existing_characters:
+                    c = Character(char_name)
+                    invalid_name = False
+                else:
+                    print("Stranger: I already know a '" + char_name + ". \nWhat is your real name?")
+            else:
+                print("Stranger: That is a very short name. \nWhat is your real name?")
+    return c
+
+
+def player_selection_menu(available_characters):
+    selection_id = 1
+    valid_selection = False
+    c = None
+    while not valid_selection:
+        for i in available_characters:
+            print(str(selection_id) + "  - " + i["name"] + " - Level " + str(i["level"]))
+            selection_id += 1
+        print(str(selection_id) + "  - New Character")
+        print("Q  - Abort")
+        user_selection = input("Enter the character number.")
+        if user_selection.upper() == "Q":
+            c = None
+            valid_selection = True
+        else:
+            try:
+                val = int(user_selection)
+                if val == selection_id:
+                    c = new_char(available_characters)
+                    valid_selection = True
+                elif 1 <= int(val) <= selection_id - 1:
+                    c = Character(available_characters[val - 1])
+                    valid_selection = True
+                else:
+                    print("Invalid Selection. Try Again.")
+            except ValueError:
+                print("Invalid Selection. Try Again.")
+    return c
+
+
+def character_selection():
+    # Read Characters Data file
+    file = open('./data/characters.json')
+    chars = json.load(file)
+    c = None
+    if len(chars) == 0:
+        c = new_char({})
+    else:
+        c = player_selection_menu(chars["characters"])
+    return c
+
 
 def main():
-    game_data = serialize_data_file()
-    game_on = True
-    while game_on:
-        player = select_character()
-
+    print("RUNA")
+    player = character_selection()
 
 
 if __name__ == '__main__':
-    main
+    main()
